9276553d6bae7fc50ba06e18ea5fc8eb0d737b9f
Akshat Runwal <61618864+runwalakshat5@users.noreply.github.com>
2022-11-12 03:04:32 -0500

    Add files via upload

---
+++ group4/lab9/lab1_svm4.R
@@ -0,0 +1,17 @@
+ ## example using the promotergene data set
+library(kernlab)
+ data(promotergene)
+
+ ## create test and training set
+ ind <- sample(1:dim(promotergene)[1],20)
+ genetrain <- promotergene[-ind, ]
+ genetest <- promotergene[ind, ]
+
+ ## train a support vector machine
+ gene <-  ksvm(Class~.,data=genetrain,kernel="rbfdot",
+               kpar=list(sigma=0.015),C=70,cross=4,prob.model=TRUE)
+
+ ## predict gene type probabilities on the test set
+ genetype <- predict(gene,genetest,type="probabilities")
+
+ 
